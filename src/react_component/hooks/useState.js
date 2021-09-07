61380a28fa94a2eb2a9df072e966b5e2963ad41b
Abdul Sohail <abdulsohail22246@gmail.com>
2021-09-07 17:30:31 +0530

    onClick function used

--- src/react_component/hooks/useState.js
+++ src/react_component/hooks/useState.js
@@ -1,24 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
 const UseState = () => {
+	const initialData = 24;
+	const [displayNum, setDisplayNum] = useState(initialData);
 	return (
 		<>
 			<div className="center_div">
-				<p>my data</p>
-				<div class="button2">
+				<p>{displayNum}</p>
+				<div class="button2" onClick={() => setDisplayNum(displayNum + 1)}>
 					<span></span>
 					<span></span>
 					<span></span>
 					<span></span>
-					Hover Me
+					Increment
 				</div>
-				<div class="button2">
+
+				<div
+					class="button2"
+					onClick={() =>
+						displayNum > 0 ? setDisplayNum(displayNum - 1) : setDisplayNum(0)
+					}
+				>
 					<span></span>
 					<span></span>
 					<span></span>
 					<span></span>
-					Hover Me
+					Decrement
 				</div>
 			</div>
 		</>
