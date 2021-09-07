e81e8a83f3c3e6f18a4a23349ebf035a4f6b6150
Abdul Sohail <abdulsohail22246@gmail.com>
2021-09-07 16:12:53 +0530

    Setting up working files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Hotel from "./react_component/basic/hotel";
+
+// Using const
+const App = () => {
+	// fat arrow function (=>)
+	// New
+	return (
+		<>
+			<Hotel />
+		</>
+	);
+};
+
+export default App;
