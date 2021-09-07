28d079b4802e345ec15c0f3e5e5f43fcc09634fe
Abdul Sohail <abdulsohail22246@gmail.com>
2021-09-07 16:52:49 +0530

    Initialized usestate js file code

--- src/react_component/hooks/useState.js
+++ src/react_component/hooks/useState.js
@@ -2,32 +2,27 @@ import React from "react";
 import "./style.css";
 
 const UseState = () => {
-  // const initialData = 15;
-  const [myNum, setMyNum] = React.useState(0);
-
-  return (
-    <>
-      <div className="center_div">
-        <p>{myNum}</p>
-        <div class="button2" onClick={() => setMyNum(myNum + 1)}>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          INCR
-        </div>
-        <div
-          class="button2"
-          onClick={() => (myNum > 0 ? setMyNum(myNum - 1) : setMyNum(0))}>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          DECR
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="center_div">
+				<p>my data</p>
+				<div class="button2">
+					<span></span>
+					<span></span>
+					<span></span>
+					<span></span>
+					Hover Me
+				</div>
+				<div class="button2">
+					<span></span>
+					<span></span>
+					<span></span>
+					<span></span>
+					Hover Me
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default UseState;
