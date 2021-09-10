352c7f57f209e772423657e3670ee4223e0e0d4d
Abdul Sohail <abdulsohail22246@gmail.com>
2021-09-10 23:10:29 +0530

    Completed the Todo list

--- src/react_component/to_do-react/todo.js
+++ src/react_component/to_do-react/todo.js
@@ -15,12 +15,26 @@ const getLocalData = () => {
 const Todo = () => {
 	const [inputData, setInputData] = useState("");
 	const [items, setItems] = useState(getLocalData());
+	const [isEditItem, setIsEditItem] = useState("");
+	const [toggleButton, setToggleButton] = useState(false);
 
 	//Function for adding Items
 
 	const addItem = () => {
 		if (!inputData) {
 			alert("Please Fill the Data");
+		} else if (inputData && toggleButton) {
+			setItems(
+				items.map((curElem) => {
+					if (curElem.id === isEditItem) {
+						return { ...curElem, name: inputData };
+					}
+					return curElem;
+				})
+			);
+			setInputData([]);
+			setIsEditItem(null);
+			setToggleButton(false);
 		} else {
 			const myNewInputData = {
 				id: new Date().getTime().toString(),
@@ -29,6 +43,16 @@ const Todo = () => {
 			setItems([...items, myNewInputData]);
 			setInputData("");
 		}
+	};
+
+	//Editing Items
+	const editItem = (index) => {
+		const item_edited = items.find((curElem) => {
+			return curElem.id === index;
+		});
+		setInputData(item_edited.name);
+		setIsEditItem(index);
+		setToggleButton(true);
 	};
 
 	// Deleting Section
@@ -65,16 +89,24 @@ const Todo = () => {
 							value={inputData}
 							onChange={(e) => setInputData(e.target.value)}
 						/>
-						<i className="fa fa-plus add-btn" onClick={addItem}></i>
+						{toggleButton ? (
+							<i className="fa fa-edit add-btn" onClick={addItem}></i>
+						) : (
+							<i className="fa fa-plus add-btn" onClick={addItem}></i>
+						)}
 					</div>
 
+					{/* To display/show items */}
 					<div className="showItems">
 						{items.map((curElem) => {
 							return (
 								<div className="eachItem" key={curElem.id}>
 									<h3>{curElem.name}</h3>
 									<div className="todo-btn">
-										<i className="fas fa-edit add-btn"></i>
+										<i
+											className="fas fa-edit add-btn"
+											onClick={() => editItem(curElem.id)}
+										></i>
 										<i
 											className="fas fa-trash-alt add-btn"
 											onClick={() => deleteItem(curElem.id)}
